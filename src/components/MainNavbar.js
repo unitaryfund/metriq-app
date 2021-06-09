9026053879dd4f97b7d48cb30ead07ba42ab0a75
Daniel Strano <stranoj@gmail.com>
2021-06-09 09:18:42 -0400

    Remove home div referencee (per Vincent's review)

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -7,7 +7,7 @@ import MainNav from "./MainNav";
 const MainNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <MainNav/>
