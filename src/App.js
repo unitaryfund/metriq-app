bac9ed83412c03d233984a0b35037cf35df729bc
Daniel Strano <stranoj@gmail.com>
2021-06-17 14:35:51 -0400

    Add anti-CSRF token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import axios from 'axios'
+import { useEffect } from 'react'
 import './App.css'
 import MainNavbar from './components/MainNavbar'
+import config from './config'
 import MainRouter from './MainRouter'
 
 const App = () => {
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get(config.api.getUriPrefix() + '/api/csrf-token')
+      axios.defaults.headers.post['X-CSRF-Token'] = data.csrfToken
+    }
+    getCsrfToken()
+  }, [])
   return (
     <div className='App'>
       <MainNavbar />
