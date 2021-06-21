e4c83072ae44d1c150da10befef7f308097ebe5c
Daniel Strano <stranoj@gmail.com>
2021-06-21 16:30:01 -0400

    #1: Redirect home on login or register

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import './App.css'
+import config from './config'
+import axios from 'axios'
+import React from 'react'
 import MainNavbar from './components/MainNavbar'
 import MainRouter from './MainRouter'
 
-const App = () => {
-  return (
-    <div className='App'>
-      <MainNavbar />
-      <MainRouter />
-    </div>
-  )
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoggedIn: false
+    }
+
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+
+    axios.get(config.api.getUriPrefix() + '/user')
+      .then(res => {
+        this.handleLogin()
+      })
+      .catch(err => {
+        if (err.response.status === 401) {
+          this.handleLogout()
+        }
+      })
+  }
+
+  handleLogin () {
+    this.setState({ isLoggedIn: true })
+  }
+
+  handleLogout () {
+    this.setState({ isLoggedIn: false })
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <MainNavbar isLoggedIn={this.state.isLoggedIn} />
+        <MainRouter isLoggedIn={this.state.isLoggedIn} onLogin={this.handleLogin} onLogout={this.handleLogout} />
+      </div>
+    )
+  }
 }
 
 export default App
