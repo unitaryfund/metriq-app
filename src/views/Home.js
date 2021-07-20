8fc608ba6fb83631da8302879f0e92ed95eb8764
Daniel Strano <stranoj@gmail.com>
2021-07-20 13:45:37 -0400

    Fix homepage view

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,13 +8,13 @@ const Home = (props) => {
       <br />
       <Tabs defaultActiveKey='trending' id='top-submissions-tabs'>
         <Tab eventKey='trending' title='Trending'>
-          <SubmissionScroll sortType='trending' isLoggedIn={props.isLoggedIn} tag={props.match.params.tag} />
+          <SubmissionScroll sortType='trending' isLoggedIn={props.isLoggedIn} tag={props.match ? props.match.params.tag : ''} />
         </Tab>
         <Tab eventKey='popular' title='Popular'>
-          <SubmissionScroll sortType='popular' isLoggedIn={props.isLoggedIn} tag={props.match.params.tag} />
+          <SubmissionScroll sortType='popular' isLoggedIn={props.isLoggedIn} tag={props.match ? props.match.params.tag : ''} />
         </Tab>
         <Tab eventKey='latest' title='Latest'>
-          <SubmissionScroll sortType='latest' isLoggedIn={props.isLoggedIn} tag={props.match.params.tag} />
+          <SubmissionScroll sortType='latest' isLoggedIn={props.isLoggedIn} tag={props.match ? props.match.params.tag : ''} />
         </Tab>
       </Tabs>
     </div>
