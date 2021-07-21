100967f9ad00239acb9e62c20b38057af26537eb
Daniel Strano <stranoj@gmail.com>
2021-07-21 13:32:50 -0400

    Submission.js basic styling

--- src/views/Submission.js
+++ src/views/Submission.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 import React from 'react'
 import config from './../config'
 import ErrorHandler from './../components/ErrorHandler'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faEdit)
 
 class Submission extends React.Component {
   constructor (props) {
@@ -24,30 +29,32 @@ class Submission extends React.Component {
 
   render () {
     return (
-      <div className='container'>
+      <div className='container submission-detail-container'>
         <header>{this.state.item.name}</header>
         <br />
         <div className='row'>
           <div className='col-md-12'>
-            <div className='submission-heading'>{this.state.item.submissionName} - Posted {this.state.item.submittedDate} {this.props.isEditView && ' - '} {this.props.isEditView && <b>{this.props.isUnderReview ? 'Under Review' : 'Approved'}</b>}</div>
+            <div><h1>{this.state.item.submissionName}</h1></div>
             <div className='submission-description'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec commodo est. Nunc mollis nunc ac ante vestibulum, eu consectetur magna porttitor. Proin ac tortor urna. Aliquam ac ante eu nunc aliquam convallis et in sem. Donec volutpat tincidunt tincidunt. Aliquam at risus non diam imperdiet vestibulum eget a orci. In ultricies, arcu vel semper lobortis, lorem orci placerat nisi, id fermentum purus odio ut nulla. Duis quis felis a erat mattis venenatis id sit amet purus. Aenean a risus dui.
             </div>
           </div>
         </div>
+        <br />
         <div className='row'>
           <div className='col-md-6'>
-            <div className='submission-heading'>Tasks</div>
+            <div><h2>Tasks <button className='float-right edit-button btn'><FontAwesomeIcon className='edit-button-icon' icon='edit' /> Edit</button></h2><hr /></div>
             <div>Lorem ipsum</div>
           </div>
           <div className='col-md-6'>
-            <div className='submission-heading'>Methods</div>
+            <div><h2>Methods <button className='float-right edit-button btn'><FontAwesomeIcon className='edit-button-icon' icon='edit' /> Edit</button></h2><hr /></div>
             <div>Lorem ipsum</div>
           </div>
         </div>
+        <br />
         <div className='row'>
           <div className='col-md-12'>
-            <div className='submission-heading'>Results</div>
+            <div><h2>Results <button className='float-right edit-button btn'><FontAwesomeIcon className='edit-button-icon' icon='edit' /> Edit</button></h2><hr /></div>
             <div>Lorem ipsum</div>
           </div>
         </div>
