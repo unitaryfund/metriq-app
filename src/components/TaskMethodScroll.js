f458922cb4a5858a5eee024d2ff31afb9c84290c
Daniel Strano <stranoj@gmail.com>
2021-08-18 21:27:15 +0000

    Remove abstract/image from Tasks index view

--- src/components/TaskMethodScroll.js
+++ src/components/TaskMethodScroll.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component'
 import FormFieldValidator from './FormFieldValidator'
 import TaskMethodBox from './TaskMethodBox'
 
@@ -17,11 +16,19 @@ class TaskMethodScroll extends React.Component {
       <div className='container'>
         <div className='row'>
           <div className='col-md-12'>
-            {this.props.items.length
-              ? (<InfiniteScroll dataLength={this.props.items.length} next={() => {}} hasMore={false} loader={<h4>Loading...</h4>} endMessage={<p style={{ textAlign: 'center' }}><b>You have seen all items.</b></p>}>{this.props.items.map((item, index) => <TaskMethodBox item={item} key={index} isLoggedIn={this.props.isLoggedIn} type={this.props.type} />)}</InfiniteScroll>)
-              : (this.props.isEditView
-                  ? <p><b>You have no items, yet.</b></p>
-                  : <p><b>There are no approved items, yet.</b></p>)}
+            {!this.props.items.length && <p><b>There are no approved items, yet.</b></p>}
+            {this.props.items.length &&
+              <div className='task'>
+                <div className='row h-100'>
+                  <div className='col-md col h-100'>
+                    <table className='task-method-item'>
+                      <tbody>
+                        {this.props.items.map((item, index) => <TaskMethodBox item={item} key={index} isLoggedIn={this.props.isLoggedIn} type={this.props.type} />)}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>}
           </div>
         </div>
         <br />
