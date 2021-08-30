70aee3938feddd446c6d889a597a546e664cbc54
Daniel Strano <stranoj@gmail.com>
2021-08-30 17:54:21 +0000

    #144: Align and style navbar elements

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -5,13 +5,13 @@ const MainNavbar = (props) => {
   return (
     <div>
       <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='/' className='navbar-title'>metriq</Navbar.Brand>
+        <Navbar.Brand href='/' className='metriq-navbar'>metriq</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <MainNav isLoggedIn={props.isLoggedIn} />
         </Navbar.Collapse>
       </Navbar>
-      <div className='bg-light navbar-title'>
+      <div className='bg-light metriq-navbar'>
         <h1>Quantum Computing Benchmarks</h1>
         <h5>by community contributors</h5>
         <br />
