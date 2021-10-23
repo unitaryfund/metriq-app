7410d719090cef428ad25cf8eec08fc72b6c7b9b
vprusso <vincentrusso1@gmail.com>
2021-10-23 09:36:49 -0400

    Fix for About in auth view.

--- src/components/AuthNav.js
+++ src/components/AuthNav.js
@@ -22,7 +22,7 @@ const AuthNav = (props) => {
       <Nav.Link href='/Methods' className='metriq-navbar-text'>Methods</Nav.Link>
       <Nav.Link href='/Tasks' className='metriq-navbar-text'>Tasks</Nav.Link>
       <Nav.Link href='/Tags' className='metriq-navbar-text'>Tags</Nav.Link>
-      <NavDropdown title='About' className='metriq-navbar-text' alignRight>
+      <NavDropdown title='About' active='true' className='metriq-navbar-text' alignRight>
         <NavDropdown.Item href='/About'><p class='font-weight-bold'>About</p></NavDropdown.Item>
         <NavDropdown.Item href='/FAQ'><p class='font-weight-bold'>F.A.Q.</p></NavDropdown.Item>
       </NavDropdown>
