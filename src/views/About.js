e0184febe003740555b2956e419d04fdb6d2f6c0
Daniel Strano <stranoj@gmail.com>
2021-10-29 15:43:41 -0400

    About: Fix CSS, justify text, block element sections

--- src/views/About.js
+++ src/views/About.js
@@ -1,85 +1,102 @@
 const About = () => {
   return (
-    <div class='container'>
-      <div CssClass='row'>
-        <div CssClass='col-md-2' />
-        <div align='left' CssClass='col-md-8'>
-          <h1>Our Mission</h1>
-          <p>
-            The mission of metriq is to create a free and open resource for acting as the state-of-the-art in categorizing benchmarks for near-term quantum computers.
-          </p>
-          <p>
-            We believe this is best done together with the community.
-          </p>
-          <p>
-            All content on this website is openly licensed under CC-BY-SA (same as Wikipedia) and everyone can contribute - look for the "Edit" buttons!
-          </p>
-          <h2>Joining the community</h2>
-          <p>
-            Join our community of contributors across academia and industry!
-          </p>
-          <p>
-            We hang out on&nbsp;
-            <a href='https://discord.com/invite/JqVGmpkP96'>
-              <span class=' icon-wrapper' data-name='discord'>
-                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-discord' viewBox='0 0 16 16'>
-                  <path d='M6.552 6.712c-.456 0-.816.4-.816.888s.368.888.816.888c.456 0 .816-.4.816-.888.008-.488-.36-.888-.816-.888zm2.92 0c-.456 0-.816.4-.816.888s.368.888.816.888c.456 0 .816-.4.816-.888s-.36-.888-.816-.888z' />
-                  <path d='M13.36 0H2.64C1.736 0 1 .736 1 1.648v10.816c0 .912.736 1.648 1.64 1.648h9.072l-.424-1.48 1.024.952.968.896L15 16V1.648C15 .736 14.264 0 13.36 0zm-3.088 10.448s-.288-.344-.528-.648c1.048-.296 1.448-.952 1.448-.952-.328.216-.64.368-.92.472-.4.168-.784.28-1.16.344a5.604 5.604 0 0 1-2.072-.008 6.716 6.716 0 0 1-1.176-.344 4.688 4.688 0 0 1-.584-.272c-.024-.016-.048-.024-.072-.04-.016-.008-.024-.016-.032-.024-.144-.08-.224-.136-.224-.136s.384.64 1.4.944c-.24.304-.536.664-.536.664-1.768-.056-2.44-1.216-2.44-1.216 0-2.576 1.152-4.664 1.152-4.664 1.152-.864 2.248-.84 2.248-.84l.08.096c-1.44.416-2.104 1.048-2.104 1.048s.176-.096.472-.232c.856-.376 1.536-.48 1.816-.504.048-.008.088-.016.136-.016a6.521 6.521 0 0 1 4.024.752s-.632-.6-1.992-1.016l.112-.128s1.096-.024 2.248.84c0 0 1.152 2.088 1.152 4.664 0 0-.68 1.16-2.448 1.216z' />
-                </svg>
-              </span> Discord
-            </a>, come join us!
-          </p>
-          <p>
-            You can also follow us and get in touch on&nbsp;
-            <a href='https://twitter.com/unitaryfund'>
-              <span class=' icon-wrapper icon-fa icon-fa-brands' data-name='twitter'>
-                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-twitter' viewBox='0 0 16 16'>
-                  <path d='M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z' />
-                </svg>
-              </span> Twitter
-            </a>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-2' />
+        <div align='left' className='col-md-8 text-justify'>
+          <h1 className='text-center'>Our Mission</h1>
+          <div>
+            <p>
+              The mission of metriq is to create a free and open resource for acting as the state-of-the-art in categorizing benchmarks for near-term quantum computers.
+            </p>
+            <p>
+              We believe this is best done together with the community.
+            </p>
+            <p>
+              All content on this website is openly licensed under CC-BY-SA (same as Wikipedia) and everyone can contribute - look for the "Edit" buttons!
+            </p>
+          </div>
+          <br />
+          <h2 className='text-center'>Joining the community</h2>
+          <div>
+            <p>
+              Join our community of contributors across academia and industry!
+            </p>
+            <p>
+              We hang out on&nbsp;
+              <a href='https://discord.com/invite/JqVGmpkP96'>
+                <span class=' icon-wrapper' data-name='discord'>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-discord' viewBox='0 0 16 16'>
+                    <path d='M6.552 6.712c-.456 0-.816.4-.816.888s.368.888.816.888c.456 0 .816-.4.816-.888.008-.488-.36-.888-.816-.888zm2.92 0c-.456 0-.816.4-.816.888s.368.888.816.888c.456 0 .816-.4.816-.888s-.36-.888-.816-.888z' />
+                    <path d='M13.36 0H2.64C1.736 0 1 .736 1 1.648v10.816c0 .912.736 1.648 1.64 1.648h9.072l-.424-1.48 1.024.952.968.896L15 16V1.648C15 .736 14.264 0 13.36 0zm-3.088 10.448s-.288-.344-.528-.648c1.048-.296 1.448-.952 1.448-.952-.328.216-.64.368-.92.472-.4.168-.784.28-1.16.344a5.604 5.604 0 0 1-2.072-.008 6.716 6.716 0 0 1-1.176-.344 4.688 4.688 0 0 1-.584-.272c-.024-.016-.048-.024-.072-.04-.016-.008-.024-.016-.032-.024-.144-.08-.224-.136-.224-.136s.384.64 1.4.944c-.24.304-.536.664-.536.664-1.768-.056-2.44-1.216-2.44-1.216 0-2.576 1.152-4.664 1.152-4.664 1.152-.864 2.248-.84 2.248-.84l.08.096c-1.44.416-2.104 1.048-2.104 1.048s.176-.096.472-.232c.856-.376 1.536-.48 1.816-.504.048-.008.088-.016.136-.016a6.521 6.521 0 0 1 4.024.752s-.632-.6-1.992-1.016l.112-.128s1.096-.024 2.248.84c0 0 1.152 2.088 1.152 4.664 0 0-.68 1.16-2.448 1.216z' />
+                  </svg>
+                </span> Discord
+              </a>, come join us!
+            </p>
+            <p>
+              You can also follow us and get in touch on&nbsp;
+              <a href='https://twitter.com/unitaryfund'>
+                <span class=' icon-wrapper icon-fa icon-fa-brands' data-name='twitter'>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-twitter' viewBox='0 0 16 16'>
+                    <path d='M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z' />
+                  </svg>
+                </span> Twitter
+              </a>
             &nbsp;and&nbsp;
-            <a href='https://github.com/unitaryfund'>
-              <span class=' icon-wrapper icon-fa icon-fa-brands' data-name='github'>
-                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-github' viewBox='0 0 16 16'>
-                  <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z' />
-                </svg>
-              </span> GitHub
-            </a>.
-          </p>
-          <h2>Contributing</h2>
-          <p>
-            Anyone can contribute - look for the "Edit" buttons!
-          </p>
-          <p>
-            Want to add an method, tag, or a task? You'll see edit buttons on the submission pages - just go ahead and edit! We found this a fun way to learn about new areas of quantum benchmarks and staying in tune with research.
-          </p>
-          <p>
-            You can make use of the <a href='https://github.com/unitaryfund/metriq-client'>metriq-client</a> project to get and submit content to the Metriq platform.
-          </p>
-          <p>
-            Please note that any contribution you make (i.e. linking code or submitting results) will be licensed under the free <a href='https://creativecommons.org/licenses/by-sa/4.0/'>CC BY-SA</a> licence.
-          </p>
-          <h2>Inclusion policy</h2>
-          <p>
-            To ensure high quality of data, all edits are monitored.
-          </p>
-          <h2>Downloading the data</h2>
-          <p>
-            All data is licensed under the <a href='https://creativecommons.org/licenses/by-sa/4.0/'>CC BY-SA</a> licence, same as Wikipedia.
-          </p>
-          <h2>Team</h2>
-          <p>
-            The core metriq team is based in the <a href='https://unitary.fund/'>Unitary Fund</a>.
-          </p>
-          <p>
-            The metriq project is heavily influenced by the <a href='https://paperswithcode.com/'>Papers with Code</a> project created by Facebook AI Research.
-          </p>
-          <p>
-            All contributions are welcome; metriq is a community project.
-          </p>
+              <a href='https://github.com/unitaryfund'>
+                <span class=' icon-wrapper icon-fa icon-fa-brands' data-name='github'>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-github' viewBox='0 0 16 16'>
+                    <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z' />
+                  </svg>
+                </span> GitHub
+              </a>.
+            </p>
+          </div>
+          <br />
+          <h2 className='text-center'>Contributing</h2>
+          <div>
+            <p>
+              Anyone can contribute - look for the "Edit" buttons!
+            </p>
+            <p>
+              Want to add an method, tag, or a task? You'll see edit buttons on the submission pages - just go ahead and edit! We found this a fun way to learn about new areas of quantum benchmarks and staying in tune with research.
+            </p>
+            <p>
+              You can make use of the <a href='https://github.com/unitaryfund/metriq-client'>metriq-client</a> project to get and submit content to the Metriq platform.
+            </p>
+            <p>
+              Please note that any contribution you make (i.e. linking code or submitting results) will be licensed under the free <a href='https://creativecommons.org/licenses/by-sa/4.0/'>CC BY-SA</a> licence.
+            </p>
+          </div>
+          <br />
+          <h2 className='text-center'>Inclusion policy</h2>
+          <div>
+            <p>
+              To ensure high quality of data, all edits are monitored.
+            </p>
+          </div>
+          <br />
+          <h2 className='text-center'>Downloading the data</h2>
+          <div>
+            <p>
+              All data is licensed under the <a href='https://creativecommons.org/licenses/by-sa/4.0/'>CC BY-SA</a> licence, same as Wikipedia.
+            </p>
+          </div>
+          <br />
+          <h2 className='text-center'>Team</h2>
+          <div>
+            <p>
+              The core metriq team is based in the <a href='https://unitary.fund/'>Unitary Fund</a>.
+            </p>
+            <p>
+              The metriq project is heavily influenced by the <a href='https://paperswithcode.com/'>Papers with Code</a> project created by Facebook AI Research.
+            </p>
+            <p>
+              All contributions are welcome; metriq is a community project.
+            </p>
+          </div>
         </div>
-        <div CssClass='col-md-2' />
+        <div className='col-md-2' />
       </div>
     </div>
   )
