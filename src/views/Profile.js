83cf26f35f1ccbd0f03e5d41057e07407a9da63f
Daniel Strano <stranoj@gmail.com>
2021-11-08 12:40:38 -0500

    #233: Fix API token info

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -39,7 +39,7 @@ class Profile extends React.Component {
           <FieldRow fieldName='username' label='Username' value={this.state.data.username} />
           <FieldRow fieldName='usernameNormal' label='Normalized Username' value={this.state.data.usernameNormal} />
           <FieldRow fieldName='email' label='Email' value={this.state.data.email} />
-          <FieldRow fieldName='clientToken' label='API Token' value={this.state.data.clientToken ? 'Active' : 'None'} />
+          <FieldRow fieldName='clientToken' label='API Token' value={this.state.data.clientTokenCreated ? 'Active' : 'None'} />
           <FieldRow fieldName='createdAt' label='Date Joined' value={this.state.data.createdAt} />
           <div className='row'>
             <div className='col-md-3' />
