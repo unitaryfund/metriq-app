7a91f552d0d11dea54ad0d12c5139663470d6b2e
Daniel Strano <stranoj@gmail.com>
2021-11-15 10:17:15 -0500

    #315: Replace Twitter icon with Discord

--- src/components/AnonNavRight.js
+++ src/components/AnonNavRight.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { FaTwitter, FaGithub } from 'react-icons/fa'
+import { FaDiscord, FaGithub } from 'react-icons/fa'
 
 import { Button, Nav } from 'react-bootstrap'
 
 const AnonNavRight = () => {
   return (
     <Nav className='ml-auto metriq-navbar'>
-      <Nav.Link href='https://twitter.com/unitaryfund'> <FaTwitter color={`${'black'}`} size={25} /> </Nav.Link>
+      <Nav.Link href='http://discord.unitary.fund'> <FaDiscord color={`${'black'}`} size={25} /> </Nav.Link>
       <Nav.Link href='https://github.com/unitaryfund'> <FaGithub color={`${'black'}`} size={25} /> </Nav.Link>
       <Nav.Link href='/Login' className='metriq-navbar-text'>Log In</Nav.Link>
       <Nav.Link href='/Login/AddSubmission'><Button variant='primary' className='metriq-navbar-button'>Submit</Button></Nav.Link>
