05ab35f91d9d95afeabc998bae66fec0d436f918
Daniel Strano <stranoj@gmail.com>
2022-02-22 15:55:21 -0500

    #439: Add tooltips w/ filter

--- src/components/SotaChart.js
+++ src/components/SotaChart.js
@@ -3,7 +3,7 @@
 // and https://betterprogramming.pub/react-d3-plotting-a-line-chart-with-tooltips-ed41a4c31f4f
 
 import React from 'react'
-import { Chart, CategoryScale, LinearScale, TimeScale, PointElement, LineElement } from 'chart.js'
+import { Chart, LinearScale, TimeScale, PointElement, LineElement, Tooltip } from 'chart.js'
 import { Line } from 'react-chartjs-2'
 import 'chartjs-adapter-moment'
 
@@ -12,17 +12,37 @@ const options = {
     x: {
       type: 'time'
     }
+  },
+  plugins: {
+    tooltip: {
+      callbacks: {
+        label: function (ctx) {
+          // console.log(ctx);
+          let label = ctx.dataset.labels[ctx.dataIndex]
+          label += ' (' + ctx.parsed.y + ')'
+          return label
+        }
+      },
+      filter: function (tooltipItem) {
+        const type = tooltipItem.dataset.type
+        if (type === 'scatter') {
+          return true
+        } else {
+          return false
+        }
+      }
+    }
   }
 }
 
 class SotaChart extends React.Component {
   constructor (props) {
     super(props)
-    Chart.register(CategoryScale)
     Chart.register(LinearScale)
     Chart.register(PointElement)
     Chart.register(LineElement)
     Chart.register(TimeScale)
+    Chart.register(Tooltip)
 
     const data = this.props.data
     const sotaData = data.length ? [data[0]] : []
@@ -42,14 +62,14 @@ class SotaChart extends React.Component {
       data: {
         datasets: [{
           type: 'scatter',
-          label: 'All',
+          labels: this.props.data.map((obj, index) => obj.method),
           backgroundColor: 'rgb(0, 0, 0)',
           borderColor: 'rgb(0, 0, 0)',
           data: this.props.data.map((obj, index) => { return { x: obj.label, y: obj.value } })
         },
         {
           type: 'line',
-          label: 'Scatter test',
+          labels: sotaData.map((obj, index) => obj.method),
           backgroundColor: 'rgb(60, 210, 249)',
           borderColor: 'rgb(60, 210, 249)',
           data: sotaData.map((obj, index) => { return { x: obj.label, y: obj.value } })
