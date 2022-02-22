12909ffe90c5a0b02d74e02f8bc289369ac557ac
Daniel Strano <stranoj@gmail.com>
2022-02-22 13:59:04 -0500

    Fix React warnings

--- src/views/Task.js
+++ src/views/Task.js
@@ -260,18 +260,14 @@ class Task extends React.Component {
                 <button className='submission-button btn btn-secondary' onClick={this.handleShowEditModal}><FontAwesomeIcon icon='edit' /></button>
               </OverlayTrigger>
               <OverlayTrigger placement='top' overlay={props => <Tooltip {...props}>Share via Facebook</Tooltip>}>
-                <button className='submission-button btn-secondary'>
-                  <FacebookShareButton url={config.api.getUriPrefix() + '/task/' + this.props.match.params.id}>
-                    <FacebookIcon size={32} />
-                  </FacebookShareButton>
-                </button>
+                <FacebookShareButton url={config.api.getUriPrefix() + '/task/' + this.props.match.params.id}>
+                  <FacebookIcon size={32} />
+                </FacebookShareButton>
               </OverlayTrigger>
               <OverlayTrigger placement='top' overlay={props => <Tooltip {...props}>Share via Twitter</Tooltip>}>
-                <button className='submission-button btn-secondary'>
-                  <TwitterShareButton url={config.api.getUriPrefix() + '/task/' + this.props.match.params.id}>
-                    <TwitterIcon size={32} />
-                  </TwitterShareButton>
-                </button>
+                <TwitterShareButton url={config.api.getUriPrefix() + '/task/' + this.props.match.params.id}>
+                  <TwitterIcon size={32} />
+                </TwitterShareButton>
               </OverlayTrigger>
             </div>
           </div>
