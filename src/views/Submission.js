9b52f8ed1fb52dae64e7710438b43bd7bcba8d97
Daniel Strano <stranoj@gmail.com>
2022-03-24 10:07:56 -0400

    #505: Confidence intervals are centered on mean

--- src/views/Submission.js
+++ src/views/Submission.js
@@ -1051,7 +1051,7 @@ class Submission extends React.Component {
                   value={this.state.result.standardError}
                   validRegex={standardErrorRegex}
                   onChange={(field, value) => this.handleOnChange('result', field, value)}
-                  tooltip='Confidence intervals will be calculated as ± standard error times z-score, if you report a standard error. This is self-consistent if your statistics are Gaussian or Poisson, for example, over a linear scale of the metric. (If Gaussian or Poisson statistics emerge over a different, non-linear scale of the metric, consider reporting your metric value with rescaled units.)'
+                  tooltip='Confidence intervals will be calculated as (mean) result metric value ± standard error times z-score, if you report a standard error. This is self-consistent if your statistics are Gaussian or Poisson, for example, over a linear scale of the metric. (If Gaussian or Poisson statistics emerge over a different, non-linear scale of the metric, consider reporting your metric value with rescaled units.)'
                 /><br />
                 <FormFieldRow
                   inputName='sampleSize' inputType='number' label='Sample size (optional)'
