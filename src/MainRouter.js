318c5cf20f88890f4b8e9636191fbd3c0de0e115
Daniel Strano <stranoj@gmail.com>
2022-03-29 11:06:43 -0400

    #510: Default 404 route

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import Home from './views/Home'
 import LogIn from './views/LogIn'
 import Register from './views/Register'
@@ -20,139 +20,143 @@ import Tags from './views/Tags'
 import Submission from './views/Submission'
 import Method from './views/Method'
 import Task from './views/Task'
+import NotFound from './views/NotFound'
 
 const MainRouter = (props) => {
   return (
     <Router>
-      <Route
-        exact
-        path='/'
-      >
-        <Home isLoggedIn={props.isLoggedIn} />
-      </Route>
-      <Route
-        exact
-        path='/Methods'
-      >
-        <Methods isLoggedIn={props.isLoggedIn} />
-      </Route>
-      <Route
-        exact
-        path='/Tasks'
-      >
-        <Tasks isLoggedIn={props.isLoggedIn} />
-      </Route>
-      <Route
-        exact
-        path='/Tags'
-      >
-        <Tags isLoggedIn={props.isLoggedIn} />
-      </Route>
-      <Route
-        exact
-        path='/Login/AddSubmission'
-      >
-        {props.isLoggedIn ? <Redirect to='/AddSubmission' /> : <LogIn onLogin={props.onLogin} nextLocation='/AddSubmission' />}
-      </Route>
-      <Route
-        exact
-        path='/Login'
-      >
-        {props.isLoggedIn ? <Redirect to='/' /> : <LogIn onLogin={props.onLogin} />}
-      </Route>
-      <Route
-        exact
-        path='/Register/AddSubmission'
-      >
-        {props.isLoggedIn ? <Redirect to='/AddSubmission' /> : <Register onLogin={props.onLogin} nextLocation='/AddSubmission' />}
-      </Route>
-      <Route
-        exact
-        path='/Register'
-      >
-        {props.isLoggedIn ? <Redirect to='/' /> : <Register onLogin={props.onLogin} />}
-      </Route>
-      <Route
-        exact
-        path='/Delete'
-      >
-        {props.isLoggedIn ? <Delete isLoggedIn onLogout={props.onLogout} /> : <Redirect to='/Login' />}
-      </Route>
-      <Route
-        exact
-        path='/Forgot'
-        component={Forgot}
-      />
-      <Route
-        exact
-        path='/Recover/:username/:uuid'
-        render={(p) => <Recover {...p} onLogin={props.onLogin} />}
-      />
-      <Route
-        exact
-        path='/About'
-        component={About}
-      />
-      <Route
-        exact
-        path='/Partners'
-        component={Partners}
-      />
-      <Route
-        exact
-        path='/FAQ'
-        component={FAQ}
-      />
-      <Route
-        exact
-        path='/Profile'
-        component={Profile}
-      />
-      <Route
-        exact
-        path='/AddSubmission'
-        render={(p) => <AddSubmission {...p} isLoggedIn={props.isLoggedIn} />}
-      />
-      <Route
-        exact
-        path='/Submissions'
-        component={Submissions}
-      />
-      <Route
-        exact
-        path='/Token'
-        component={Token}
-      />
-      <Route
-        exact
-        path='/Password'
-        component={Password}
-      />
-      <Route
-        exact
-        path='/Tag/:tag'
-        render={(p) => <Home {...p} onLogin={props.onLogin} />}
-      />
-      <Route
-        exact
-        path='/Submission/:id'
-        render={(p) => <Submission {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
-      />
-      <Route
-        exact
-        path='/User/:userId/Submissions'
-        render={(p) => <SubmissionsPublic {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
-      />
-      <Route
-        exact
-        path='/Method/:id'
-        render={(p) => <Method {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
-      />
-      <Route
-        exact
-        path='/Task/:id'
-        render={(p) => <Task {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
-      />
+      <Switch>
+        <Route
+          exact
+          path='/'
+        >
+          <Home isLoggedIn={props.isLoggedIn} />
+        </Route>
+        <Route
+          exact
+          path='/Methods'
+        >
+          <Methods isLoggedIn={props.isLoggedIn} />
+        </Route>
+        <Route
+          exact
+          path='/Tasks'
+        >
+          <Tasks isLoggedIn={props.isLoggedIn} />
+        </Route>
+        <Route
+          exact
+          path='/Tags'
+        >
+          <Tags isLoggedIn={props.isLoggedIn} />
+        </Route>
+        <Route
+          exact
+          path='/Login/AddSubmission'
+        >
+          {props.isLoggedIn ? <Redirect to='/AddSubmission' /> : <LogIn onLogin={props.onLogin} nextLocation='/AddSubmission' />}
+        </Route>
+        <Route
+          exact
+          path='/Login'
+        >
+          {props.isLoggedIn ? <Redirect to='/' /> : <LogIn onLogin={props.onLogin} />}
+        </Route>
+        <Route
+          exact
+          path='/Register/AddSubmission'
+        >
+          {props.isLoggedIn ? <Redirect to='/AddSubmission' /> : <Register onLogin={props.onLogin} nextLocation='/AddSubmission' />}
+        </Route>
+        <Route
+          exact
+          path='/Register'
+        >
+          {props.isLoggedIn ? <Redirect to='/' /> : <Register onLogin={props.onLogin} />}
+        </Route>
+        <Route
+          exact
+          path='/Delete'
+        >
+          {props.isLoggedIn ? <Delete isLoggedIn onLogout={props.onLogout} /> : <Redirect to='/Login' />}
+        </Route>
+        <Route
+          exact
+          path='/Forgot'
+          component={Forgot}
+        />
+        <Route
+          exact
+          path='/Recover/:username/:uuid'
+          render={(p) => <Recover {...p} onLogin={props.onLogin} />}
+        />
+        <Route
+          exact
+          path='/About'
+          component={About}
+        />
+        <Route
+          exact
+          path='/Partners'
+          component={Partners}
+        />
+        <Route
+          exact
+          path='/FAQ'
+          component={FAQ}
+        />
+        <Route
+          exact
+          path='/Profile'
+          component={Profile}
+        />
+        <Route
+          exact
+          path='/AddSubmission'
+          render={(p) => <AddSubmission {...p} isLoggedIn={props.isLoggedIn} />}
+        />
+        <Route
+          exact
+          path='/Submissions'
+          component={Submissions}
+        />
+        <Route
+          exact
+          path='/Token'
+          component={Token}
+        />
+        <Route
+          exact
+          path='/Password'
+          component={Password}
+        />
+        <Route
+          exact
+          path='/Tag/:tag'
+          render={(p) => <Home {...p} onLogin={props.onLogin} />}
+        />
+        <Route
+          exact
+          path='/Submission/:id'
+          render={(p) => <Submission {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
+        />
+        <Route
+          exact
+          path='/User/:userId/Submissions'
+          render={(p) => <SubmissionsPublic {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
+        />
+        <Route
+          exact
+          path='/Method/:id'
+          render={(p) => <Method {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
+        />
+        <Route
+          exact
+          path='/Task/:id'
+          render={(p) => <Task {...p} isLoggedIn={props.isLoggedIn} onLogin={props.onLogin} />}
+        />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   )
 }
