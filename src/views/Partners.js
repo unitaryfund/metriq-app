c4605e8eec1b9db0262bd2cc6f8b90108296975c
Daniel Strano <stranoj@gmail.com>
2022-04-29 15:15:12 -0400

    #530: Move <br/> between partner logo rows

--- src/views/Partners.js
+++ src/views/Partners.js
@@ -21,8 +21,8 @@ const Partners = () => {
             </p>
             <div className='text-center'>
               <a href='https://www.iqt.org/'><img src={iqtLogo} alt='IQT partner logo' className='logo-image' /></a>
-              <a href='https://unitary.fund/'><img src={ufLogo} alt='UF partner logo' className='logo-image' /></a><br />
-              <a href='https://riacs.usra.edu/quantum/nisqc-nl'><img src={usraLogo} alt='USRA partner logo' className='logo-image' /></a>
+              <a href='https://unitary.fund/'><img src={ufLogo} alt='UF partner logo' className='logo-image' /></a>
+              <a href='https://riacs.usra.edu/quantum/nisqc-nl'><img src={usraLogo} alt='USRA partner logo' className='logo-image' /></a><br />
               <a href='https://cambridgequantum.com/'><img src={cqcLogo} alt='Cambridge Quantum Computing partner logo' className='logo-image' /></a>
               <a href='https://www.riken.jp/en/'><img src={rikenLogo} alt='Riken University logo' className='logo-image' /></a>
               <a href='https://strangeworks.com/'><img src={strangeworksLogo} alt='Strangeworks logo' className='logo-image' /></a>
