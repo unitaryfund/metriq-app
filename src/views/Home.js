7c91a358659914fa5920e9038205a33094193d30
Daniel Strano <stranoj@gmail.com>
2022-06-30 15:27:27 -0400

    #608: Prevent infinite scroll on link to homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,11 @@
+import { useEffect } from 'react'
 import { Tabs, Tab } from 'react-bootstrap'
 import SubmissionScroll from '../components/SubmissionScroll'
 
 const Home = (props) => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <div id='metriq-main-content' className='container'>
       <header><h4>Top Submissions {props.match ? 'for "' + props.match.params.tag + '"' : ''}</h4></header>
