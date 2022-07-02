9ad294fef16fd86a8f6c5bc8456fe07e6e890a64
Daniel Strano <stranoj@gmail.com>
2022-07-01 23:09:55 -0400

    Reuse CategoryItemIcon

---
+++ src/components/CategoryItemIcon.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const CategoryItemBox = (props) => {
+  return (
+    <div className='col-4 col-md-1'>
+      <OverlayTrigger placement='top' overlay={props => <Tooltip {...props}>Count of {props.word}, with {props.type}</Tooltip>}>
+        <span><FontAwesomeIcon icon={props.icon} /><br />{props.count}</span>
+      </OverlayTrigger>
+    </div>
+  )
+}
+
+export default CategoryItemBox
