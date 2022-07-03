6106901da5c38964fa41bbf1aed25d982b79a81d
Daniel Strano <stranoj@gmail.com>
2022-07-03 09:42:10 -0400

    App.js as React function

--- src/App.js
+++ src/App.js
@@ -1,67 +1,54 @@
 import './App.css'
 import config from './config'
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import MainNavbar from './components/MainNavbar'
 import MainRouter from './MainRouter'
 import SimpleReactFooter from './components/simple-react-footer/SimpleReactFooter'
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      isLoggedIn: false,
-      userId: ''
-    }
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-    this.handleLogin = this.handleLogin.bind(this)
-    this.handleLogout = this.handleLogout.bind(this)
+  const handleLogin = () => {
+    setIsLoggedIn(true)
   }
 
-  componentDidMount () {
-    axios.get(config.api.getUriPrefix() + '/user')
-      .then(res => {
-        this.handleLogin()
-      })
-      .catch(err => {
-        if (!err || !err.response || err.response.status === 401) {
-          this.handleLogout()
-        }
-      })
+  const handleLogout = () => {
+    setIsLoggedIn(false)
   }
 
-  handleLogin () {
-    this.setState({ isLoggedIn: true })
-  }
+  axios.get(config.api.getUriPrefix() + '/user')
+    .then(res => {
+      handleLogin()
+    })
+    .catch(err => {
+      if (!err || !err.response || err.response.status === 401) {
+        handleLogout()
+      }
+    })
 
-  handleLogout () {
-    this.setState({ isLoggedIn: false })
-  }
-
-  render () {
-    return (
-      <div className='App'>
-        <MainNavbar isLoggedIn={this.state.isLoggedIn} title='Community-driven Quantum Benchmarks' subtitle={<span><a href='/'>Submissions</a> show performance of <a href='/Methods/'>methods</a> against <a href='/Tasks/'>tasks</a></span>} />
-        <MainRouter id='metriq-main-content' isLoggedIn={this.state.isLoggedIn} onLogin={this.handleLogin} onLogout={this.handleLogout} />
-        <div className='footer-shim' />
-        <SimpleReactFooter
-          title='metriq'
-          description='Quantum computing benchmarks by community contributors'
-          copyright='2021, 2022 Unitary Fund'
-          discord='unitary.fund'
-          github='unitaryfund'
-          twitch='unitaryfund'
-          twitter='unitaryfund'
-          youtube='UCDbDLAzGRTHnhkoMMOX7D1A'
-          linkedin='unitary-fund'
-          backgroundColor='#212529'
-          fontColor='#FFFFFF'
-          iconColor='#FFFFFF'
-          columns={[]}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className='App'>
+      <MainNavbar isLoggedIn={isLoggedIn} title='Community-driven Quantum Benchmarks' subtitle={<span><a href='/'>Submissions</a> show performance of <a href='/Methods/'>methods</a> against <a href='/Tasks/'>tasks</a></span>} />
+      <MainRouter id='metriq-main-content' isLoggedIn={isLoggedIn} onLogin={handleLogin} onLogout={handleLogout} />
+      <div className='footer-shim' />
+      <SimpleReactFooter
+        title='metriq'
+        description='Quantum computing benchmarks by community contributors'
+        copyright='2021, 2022 Unitary Fund'
+        discord='unitary.fund'
+        github='unitaryfund'
+        twitch='unitaryfund'
+        twitter='unitaryfund'
+        youtube='UCDbDLAzGRTHnhkoMMOX7D1A'
+        linkedin='unitary-fund'
+        backgroundColor='#212529'
+        fontColor='#FFFFFF'
+        iconColor='#FFFFFF'
+        columns={[]}
+      />
+    </div>
+  )
 }
 
 export default App
