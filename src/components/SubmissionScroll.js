da5a60782eb3518cfab82551f81ce0e27b72f284
Daniel Strano <stranoj@gmail.com>
2022-07-02 22:30:40 -0400

    SubmissionScroll class might be preferable

--- src/components/SubmissionScroll.js
+++ src/components/SubmissionScroll.js
@@ -1,61 +1,66 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React from 'react'
 import config from './../config'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import FormFieldValidator from '../components/FormFieldValidator'
 import SubmissionBox from '../components/SubmissionBox'
 import ErrorHandler from './ErrorHandler'
-import FormFieldTypeaheadRow from './FormFieldTypeaheadRow'
 import FormFieldAlertRow from './FormFieldAlertRow'
 import FormFieldWideRow from './FormFieldWideRow'
 
-const SubmissionScroll = (props) => {
-  const [nextPage, setNextPage] = useState(0)
-  const [filterText, setFilterText] = useState('')
-  const [filterOptions, setFilterOptions] = useState([])
-  const [items, setItems] = useState([])
-  const [filteredItems, setFilteredItems] = useState([])
-  const [hasMore, setHasMore] = useState(true)
-  const [requestFailedMessage, setRequestFailedMessage] = useState('')
+class SubmissionScroll extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      nextPage: 0,
+      filterText: '',
+      filterOptions: [],
+      items: [],
+      filteredItems: [],
+      hasMore: true,
+      isRequestFailed: false,
+      requestFailedMessage: ''
+    }
 
-  const route = props.isEditView
-    ? config.api.getUriPrefix() + '/user/submission/0'
-    : (props.isPublicView
-        ? config.api.getUriPrefix() + '/user/' + props.userId + '/submission/0'
-        : (props.tag
-            ? config.api.getUriPrefix() + '/submission/' + props.tag + '/' + props.sortType + '/0'
-            : config.api.getUriPrefix() + '/submission/' + props.sortType + '/0'))
+    this.fetchMoreData = this.fetchMoreData.bind(this)
+    this.onFilter = this.onFilter.bind(this)
+  }
 
-  axios.get(route)
-    .then(res => {
-      const items = res.data.data
-      const filterOptions = []
-      for (let i = 0; i < items.length; i++) {
-        filterOptions.push(items[i].name)
-        filterOptions.push(items[i].contentUrl)
-      }
-      setRequestFailedMessage('')
-      setNextPage(1)
-      setItems(items)
-      setFilterOptions(filterOptions)
-      setFilteredItems(items)
-    })
-    .catch(err => {
-      setRequestFailedMessage(ErrorHandler(err))
-    })
+  componentDidMount () {
+    const route = this.props.isEditView
+      ? config.api.getUriPrefix() + '/user/submission/0'
+      : (this.props.isPublicView
+          ? config.api.getUriPrefix() + '/user/' + this.props.userId + '/submission/0'
+          : (this.props.tag
+              ? config.api.getUriPrefix() + '/submission/' + this.props.tag + '/' + this.props.sortType + '/0'
+              : config.api.getUriPrefix() + '/submission/' + this.props.sortType + '/0'))
 
-  const fetchMoreData = () => {
-    const route = props.isEditView
-      ? config.api.getUriPrefix() + '/user/submission/' + nextPage
-      : (props.isPublicView
-          ? config.api.getUriPrefix() + '/user/' + props.userId + '/submission/' + nextPage
-          : (props.tag
-              ? config.api.getUriPrefix() + '/submission/' + props.tag + '/' + props.sortType + '/' + nextPage
-              : config.api.getUriPrefix() + '/submission/' + props.sortType + '/' + nextPage))
     axios.get(route)
       .then(res => {
-        setRequestFailedMessage('')
-        setNextPage(nextPage + 1)
+        const items = res.data.data
+        const filterOptions = []
+        for (let i = 0; i < items.length; i++) {
+          filterOptions.push(items[i].name)
+          filterOptions.push(items[i].contentUrl)
+        }
+        this.setState({ isRequestFailed: false, requestFailedMessage: '', nextPage: 1, items: items, filterOptions: filterOptions, filteredItems: items })
+      })
+      .catch(err => {
+        this.setState({ isRequestFailed: true, requestFailedMessage: ErrorHandler(err) })
+      })
+  }
+
+  fetchMoreData () {
+    const route = this.props.isEditView
+      ? config.api.getUriPrefix() + '/user/submission/' + this.state.nextPage
+      : (this.props.isPublicView
+          ? config.api.getUriPrefix() + '/user/' + this.props.userId + '/submission/' + this.state.nextPage
+          : (this.props.tag
+              ? config.api.getUriPrefix() + '/submission/' + this.props.tag + '/' + this.props.sortType + '/' + this.state.nextPage
+              : config.api.getUriPrefix() + '/submission/' + this.props.sortType + '/' + this.state.nextPage))
+    axios.get(route)
+      .then(res => {
+        this.setState({ isRequestFailed: false, requestFailedMessage: '', nextPage: this.state.nextPage + 1 })
         const items = res.data.data
         const filterOptions = []
         for (let i = 0; i < items.length; i++) {
@@ -63,84 +68,74 @@ const SubmissionScroll = (props) => {
           filterOptions.push(items[i].contentUrl)
         }
         if (items.length > 0) {
-          setItems(items.concat(items))
-          setFilterOptions(filterOptions.concat(filterOptions))
+          this.setState({ items: this.state.items.concat(items), filterOptions: this.state.filterOptions.concat(filterOptions) })
         } else {
-          setHasMore(false)
+          this.setState({ hasMore: false })
         }
 
-        this.onFilter(filterText)
+        this.onFilter(this.state.filterText)
       })
       .catch(err => {
-        setRequestFailedMessage(ErrorHandler(err))
+        this.setState({ isRequestFailed: true, requestFailedMessage: ErrorHandler(err) })
       })
   }
 
-  const onFilter = (text) => {
+  onFilter (text) {
     if (!text || (text.length < 3)) {
-      setFilterText(text)
-      setFilteredItems(items)
+      this.setState({ filterText: text, filteredItems: this.state.items })
       return
     }
 
     text = text.toLowerCase()
 
     const fItems = []
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i]
+    for (let i = 0; i < this.state.items.length; i++) {
+      const item = this.state.items[i]
       if (item.name.toLowerCase().includes(text) || item.contentUrl.toLowerCase().includes(text)) {
         fItems.push(item)
       }
     }
 
-    if (hasMore && fItems.length === 0) {
+    if (this.state.hasMore && fItems.length === 0) {
       this.fetchMoreData()
     }
 
-    setFilterText(text)
-    setFilteredItems(fItems)
+    this.setState({ filterText: text, filteredItems: fItems })
   }
 
-  return (
-    <div className='container'>
-      <FormFieldWideRow className='search-bar'>
-        <FormFieldTypeaheadRow
-          options={filterOptions}
-          inputName='nameOrUrl'
-          label='Search title or URL'
-          value=''
-          onChange={(field, value) => onFilter(value)}
-        />
-      </FormFieldWideRow>
-      <FormFieldWideRow>
-        {items.length && (
-          <InfiniteScroll
-            dataLength={filteredItems.length}
-            next={fetchMoreData}
-            hasMore={hasMore}
-            loader={<h4>Loading...</h4>}
-            endMessage={<p style={{ textAlign: 'center' }}><b>You have seen all submissions.</b></p>}
-          >
-            {filteredItems.map((item, index) =>
-              <SubmissionBox
-                item={item}
-                key={index}
-                isLoggedIn={props.isLoggedIn}
-                isEditView={props.isEditView}
-                isUnderReview={!(item.approvedAt)}
-              />)}
-          </InfiniteScroll>
-        )}
-        {!items.length && (props.isEditView
-          ? <p><b>You have no submissions, yet.</b></p>
-          : <p><b>There are no approved submissions, yet.</b></p>)}
-      </FormFieldWideRow>
-      <br />
-      <FormFieldAlertRow>
-        <FormFieldValidator invalid={!!requestFailedMessage} message={requestFailedMessage} />
-      </FormFieldAlertRow>
-    </div>
-  )
+  render () {
+    return (
+      <div className='container'>
+        <FormFieldWideRow>
+          {this.state.items.length && (
+            <InfiniteScroll
+              dataLength={this.state.filteredItems.length}
+              next={this.fetchMoreData}
+              hasMore={this.state.hasMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={<p style={{ textAlign: 'center' }}><b>You have seen all submissions.</b></p>}
+            >
+              {this.state.filteredItems.map((item, index) =>
+                <SubmissionBox
+                  item={item}
+                  key={index}
+                  isLoggedIn={this.props.isLoggedIn}
+                  isEditView={this.props.isEditView}
+                  isUnderReview={!(item.approvedAt)}
+                />)}
+            </InfiniteScroll>
+          )}
+          {!this.state.items.length && (this.props.isEditView
+            ? <p><b>You have no submissions, yet.</b></p>
+            : <p><b>There are no approved submissions, yet.</b></p>)}
+        </FormFieldWideRow>
+        <br />
+        <FormFieldAlertRow>
+          <FormFieldValidator invalid={this.state.isRequestFailed} message={this.state.requestFailedMessage} />
+        </FormFieldAlertRow>
+      </div>
+    )
+  }
 }
 
 export default SubmissionScroll
