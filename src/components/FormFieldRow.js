1f7f50e96adeb5133431d33ab96e43b629fc08e5
Daniel Strano <stranoj@gmail.com>
2022-07-11 15:31:08 -0400

    Fix metadata population

--- src/components/FormFieldRow.js
+++ src/components/FormFieldRow.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import FormFieldValidator from './FormFieldValidator'
 import FormFieldWideRow from './FormFieldWideRow'
 import TooltipTrigger from './TooltipTrigger'
 
 const FormFieldRow = (props) => {
-  const [value, setValue] = useState(props.value ? props.value : (props.defaultValue ? props.defaultValue : ''))
+  const [value, setValue] = useState(props.value)
   const [checked, setChecked] = useState(props.checked ? props.checked : false)
   const [isValid, setIsValid] = useState(true)
   const [imagePreviewUrl, setImagePreviewUrl] = useState('')
+
+  useEffect(() => { setValue(props.value) }, [props.value])
 
   const handleOnFieldChange = (event) => {
     // For a regular input field, read field name and value from the event.
@@ -23,12 +25,10 @@ const FormFieldRow = (props) => {
     if (props.validRegex) {
       setIsValid(props.validRegex.test(fieldValue))
     }
-    if (fieldValue !== value) {
-      if (props.onChange) {
-        props.onChange(fieldName, fieldValue)
-      }
-      setValue(fieldValue)
+    if (props.onChange) {
+      props.onChange(fieldName, fieldValue)
     }
+    setValue(fieldValue)
   }
 
   const handleOnFieldBlur = (event) => {
@@ -37,12 +37,10 @@ const FormFieldRow = (props) => {
     if (props.validRegex) {
       setIsValid(props.validRegex.test(fieldValue))
     }
-    if (fieldValue !== value) {
-      if (props.onBlur) {
-        props.onBlur(fieldName, fieldValue)
-      }
-      setValue(fieldValue)
+    if (props.onBlur) {
+      props.onBlur(fieldName, fieldValue)
     }
+    setValue(fieldValue)
   }
 
   return (
