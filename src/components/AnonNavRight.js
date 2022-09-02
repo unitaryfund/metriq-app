50b743ff3520611d95aa66aa52cec2c8b7bc5058
vprusso <vincentrusso1@gmail.com>
2022-09-02 09:11:41 -0400

    Adding metriqinfo twitter icon in the top right.

--- src/components/AnonNavRight.js
+++ src/components/AnonNavRight.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { FaDiscord, FaGithub } from 'react-icons/fa'
+import { FaDiscord, FaGithub, FaTwitter } from 'react-icons/fa'
 import { Button, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const AnonNavRight = () =>
   <Nav className='ml-auto metriq-navbar'>
+    <Nav.Link href='https://twitter.com/MetriqInfo'> <FaTwitter color={`${'black'}`} size={25} /> </Nav.Link>
     <Nav.Link href='http://discord.unitary.fund'> <FaDiscord color={`${'black'}`} size={25} /> </Nav.Link>
     <Nav.Link href='https://github.com/unitaryfund/metriq-app'> <FaGithub color={`${'black'}`} size={25} /> </Nav.Link>
     <Nav.Link as={Link} to='/Login' className='metriq-navbar-text'>Log In</Nav.Link>
