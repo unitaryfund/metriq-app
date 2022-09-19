97d750a2e5f73ff37c8f84fd9ab0834b4f7cc132
Daniel Strano <stranoj@gmail.com>
2022-09-19 16:20:09 -0400

    #155: Highlight active NavLink in react-router-dom

--- src/components/MainNavLeft.js
+++ src/components/MainNavLeft.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Nav, NavDropdown } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const MainNavLeft = () =>
   <Nav className='metriq-navbar'>
-    <Nav.Link as={Link} to='/Tasks' className='metriq-navbar-text'>Tasks</Nav.Link>
-    <Nav.Link as={Link} to='/Methods' className='metriq-navbar-text'>Methods</Nav.Link>
-    <Nav.Link as={Link} to='/Platforms' className='metriq-navbar-text'>Platforms</Nav.Link>
-    <Nav.Link as={Link} to='/Tags' className='metriq-navbar-text'>Tags</Nav.Link>
-    <Nav.Link as={Link} to='/Submissions' className='metriq-navbar-text'>Submissions</Nav.Link>
+    <Nav.Link as={NavLink} activeClassName='active-navlink' to='/Tasks' className='metriq-navbar-text'>Tasks</Nav.Link>
+    <Nav.Link as={NavLink} activeClassName='active-navlink' to='/Methods' className='metriq-navbar-text'>Methods</Nav.Link>
+    <Nav.Link as={NavLink} activeClassName='active-navlink' to='/Platforms' className='metriq-navbar-text'>Platforms</Nav.Link>
+    <Nav.Link as={NavLink} activeClassName='active-navlink' to='/Tags' className='metriq-navbar-text'>Tags</Nav.Link>
+    <Nav.Link as={NavLink} activeClassName='active-navlink' to='/Submissions' className='metriq-navbar-text'>Submissions</Nav.Link>
     <NavDropdown title='About' active='true' className='metriq-navbar-text' alignRight>
-      <NavDropdown.Item as={Link} to='/About'><p class='font-weight-bold'>About</p></NavDropdown.Item>
-      <NavDropdown.Item as={Link} to='/Partners'><p class='font-weight-bold'>Partners</p></NavDropdown.Item>
-      <NavDropdown.Item as={Link} to='/FAQ'><p class='font-weight-bold'>F.A.Q.</p></NavDropdown.Item>
-      <NavDropdown.Item as={Link} to='/UserGuidelines'><p class='font-weight-bold'>User Guidelines</p></NavDropdown.Item>
+      <NavDropdown.Item as={NavLink} activeClassName='active-navlink' to='/About'><p class='font-weight-bold'>About</p></NavDropdown.Item>
+      <NavDropdown.Item as={NavLink} activeClassName='active-navlink' to='/Partners'><p class='font-weight-bold'>Partners</p></NavDropdown.Item>
+      <NavDropdown.Item as={NavLink} activeClassName='active-navlink' to='/FAQ'><p class='font-weight-bold'>F.A.Q.</p></NavDropdown.Item>
+      <NavDropdown.Item as={NavLink} activeClassName='active-navlink' to='/UserGuidelines'><p class='font-weight-bold'>User Guidelines</p></NavDropdown.Item>
     </NavDropdown>
   </Nav>
 
