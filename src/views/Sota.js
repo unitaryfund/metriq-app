a6e26748c75b725088220dc732dbe85e5f83a36a
Daniel Strano <stranoj@gmail.com>
2023-11-30 10:29:53 -0500

    Update SOTA text

--- src/views/Sota.js
+++ src/views/Sota.js
@@ -57,9 +57,10 @@ const Sota = (props) => {
       </div>
       <div className='row'>
         <div className='col-md-9 text-left'>
-          <p>We are still in the <b>"noisy intermediate-scale quantum" (NISQ)</b> era of quantum computing: qubit count and quality remain too low for practical error correction schemes. Therefore, we must still use our "cleanest" subset of hardware qubits as our logical qubits, without error correction. <Link to={'/Platform/80'}><b>Quantinuum System Model H1-1</b></Link> has the highest quantum volume in the industry, equivalent to approximately <Link to={'/Submission/642'}><b>19 "algorithmic qubits,"</b></Link> without error correction.</p>
-          <p><b>Spin qubits</b> based on silicon, as well as <b>neutral atom qubits</b>, lead the field for <b>T2 coherence time,</b> up to about <Link to={'/Submission/589'}><b>21 seconds</b></Link> before these qubits entirely lose their original computational state.</p>
-          <p>Proof-of-concept of <b>error correction</b> has been achieved up to a <b>coherence gain</b> multiplicative factor of about <Link to={'/Submission/463'}><b>5.1</b></Link>, meaning that these test systems achieved 5.1 times the coherence time of their hardware qubits.</p>
+          <p><Link to='/Platform/80'><b>Quantinuum System Model H1-1</b></Link>, based on the <Link to='/Architecture/2'><b>Ion architecture</b></Link>, has the highest <Link to='/Submission/642'><b>quantum volume of  2^19</b></Link> in the industry, equivalent to <b>19 "algorithmic qubits,"</b> without error correction.</p>
+          <p>We are still in the <b>"noisy intermediate-scale quantum" (NISQ)</b> era of quantum computing: qubit count and quality remain too low for practical error correction schemes. Therefore, we must still use our "cleanest" subset of hardware qubits as our logical qubits, without error correction.</p>
+          <p><b>Spin qubits</b> based on silicon, as well as <b>neutral atom qubits</b>, lead the field for <b>T2 coherence time,</b> up to about <Link to='/Submission/589'><b>21 seconds</b></Link> before these qubits entirely lose their original computational state.</p>
+          <p>Proof-of-concept of <b>error correction</b> has been achieved up to a <b>coherence gain</b> multiplicative factor of about <Link to='/Submission/463'><b>5.1 with the Star Code</b></Link>, meaning that these test systems achieved 5.1 times the coherence time of their hardware qubits.</p>
         </div>
       </div>
     </div>
