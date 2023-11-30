ff054c952abfdaeb701d1ae15d64e085064f1a62
Daniel Strano <stranoj@gmail.com>
2023-11-30 10:54:01 -0500

    Update SOTA text

--- src/views/Sota.js
+++ src/views/Sota.js
@@ -9,6 +9,27 @@ const Sota = (props) => {
           <h4 align='left'>State of the Art</h4>
         </div>
       </div>
+      <br />
+      <div className='row'>
+        <div className='col'>
+          <h5 align='left'>Summary</h5>
+        </div>
+      </div>
+      <br />
+      <div className='row'>
+        <div className='col-md-9 text-left'>
+          <p><Link to='/Platform/80'><b>Quantinuum System Model H1-1</b></Link>, based on the <Link to='/Architecture/2'><b>Ion architecture</b></Link>, has the highest <Link to='/Submission/642'><b>quantum volume of  2^19</b></Link> in the industry, equivalent to <b>19 "algorithmic qubits,"</b> without error correction. We are still in the <b>"noisy intermediate-scale quantum" (NISQ)</b> era of quantum computing: qubit count and quality remain too low for practical error correction schemes. Therefore, we must still use our "cleanest" subset of hardware qubits as our logical qubits, without error correction.</p>
+          <p><Link to='/Architecture/4'><b>Spin qubits</b></Link> based on silicon, as well as <Link to='/Architrecture/1'><b>neutral atom qubits</b></Link>, lead the field for <Link to='/Task/50'><b>T2 coherence time,</b></Link> up to about <Link to='/Submission/589'><b>21 seconds</b></Link> before these qubits entirely lose their original computational state.</p>
+          <p>Proof-of-concept of <b>error correction</b> has been achieved up to a <Link to='/Task/164'><b>coherence gain</b></Link> multiplicative factor of about <Link to='/Submission/463'><b>5.1 with the Star Code</b></Link>, meaning that these test systems achieved 5.1 times the coherence time of their hardware qubits.</p>
+        </div>
+      </div>
+      <br />
+      <div className='row'>
+        <div className='col'>
+          <h5 align='left'>Statistics</h5>
+        </div>
+      </div>
+      <br />
       <div className='row'>
         <div className='col-md-9 text-left'>
           The current highest quantum volume across the industry is <Link to='/Submission/642'><b>2^19</b></Link> by <Link to='/Platform/80'><b>Quantinuum System Model H1-1</b></Link>.
@@ -55,14 +76,6 @@ const Sota = (props) => {
             isLoggedIn={props.isLoggedIn}
           />
           <br />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col-md-9 text-left'>
-          <p><Link to='/Platform/80'><b>Quantinuum System Model H1-1</b></Link>, based on the <Link to='/Architecture/2'><b>Ion architecture</b></Link>, has the highest <Link to='/Submission/642'><b>quantum volume of  2^19</b></Link> in the industry, equivalent to <b>19 "algorithmic qubits,"</b> without error correction.</p>
-          <p>We are still in the <b>"noisy intermediate-scale quantum" (NISQ)</b> era of quantum computing: qubit count and quality remain too low for practical error correction schemes. Therefore, we must still use our "cleanest" subset of hardware qubits as our logical qubits, without error correction.</p>
-          <p><Link to='/Architecture/4'><b>Spin qubits</b></Link> based on silicon, as well as <Link to='/Architrecture/1'><b>neutral atom qubits</b></Link>, lead the field for <Link to='/Task/50'><b>T2 coherence time,</b></Link> up to about <Link to='/Submission/589'><b>21 seconds</b></Link> before these qubits entirely lose their original computational state.</p>
-          <p>Proof-of-concept of <b>error correction</b> has been achieved up to a <Link to='/Task/164'><b>coherence gain</b></Link> multiplicative factor of about <Link to='/Submission/463'><b>5.1 with the Star Code</b></Link>, meaning that these test systems achieved 5.1 times the coherence time of their hardware qubits.</p>
         </div>
       </div>
     </div>
