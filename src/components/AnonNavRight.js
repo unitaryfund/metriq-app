8216e958c44a5ac6195b3951054371795e919cc8
WrathfulSpatula <stranoj@gmail.com>
2025-02-04 10:15:52 -0500

    #944: Fix header bar alignment

--- src/components/AnonNavRight.js
+++ src/components/AnonNavRight.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Button, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { FaDiscord, FaGithub, FaTwitter } from 'react-icons/fa'
 import MainSearchBar from './MainSearchBar'
 
 const AnonNavRight = () =>
   <Nav className='ml-auto metriq-navbar metriq-navbar-right'>
+    <Nav.Link href='https://twitter.com/MetriqInfo' eventKey='8'> <FaTwitter color={`${'white'}`} size={25} className='metriq-navbar-social-icon' /> </Nav.Link>
+      <Nav.Link href='http://discord.unitary.fund' eventKey='9'> <FaDiscord color={`${'white'}`} size={25} className='metriq-navbar-social-icon' /> </Nav.Link>
+      <Nav.Link href='https://github.com/unitaryfund/metriq-app' eventKey='10'> <FaGithub color={`${'white'}`} size={25} className='metriq-navbar-social-icon' /> </Nav.Link>
     <MainSearchBar />
-    <Nav.Link as={Link} to='/Login' eventKey='13'><Button variant='outline-light' className='metriq-navbar-button'>Log In</Button></Nav.Link>
-    <Nav.Link as={Link} to='/Login/AddSubmission' eventKey='14'><Button variant='primary' className='metriq-navbar-button submit-button'>Submit</Button></Nav.Link>
+    <Nav.Link as={Link} to='/Login' eventKey='11'><Button variant='outline-light' className='metriq-navbar-button'>Log In</Button></Nav.Link>
+    <Nav.Link as={Link} to='/Login/AddSubmission' eventKey='12'><Button variant='primary' className='metriq-navbar-button submit-button'>Submit</Button></Nav.Link>
   </Nav>
 
 export default AnonNavRight
--- src/components/AuthNavRight.js
+++ src/components/AuthNavRight.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import config from './../config'
 import ErrorHandler from './ErrorHandler'
 import { Link } from 'react-router-dom'
+import { FaDiscord, FaGithub, FaTwitter } from 'react-icons/fa'
 import MainSearchBar from './MainSearchBar'
 
 const handleOnClick = () => {
@@ -16,17 +17,20 @@ const handleOnClick = () => {
 
 const AuthNavRight = () =>
   <Nav className='ml-auto metriq-navbar metriq-navbar-right'>
+    <Nav.Link href='https://twitter.com/MetriqInfo' eventKey='8'> <FaTwitter color={`${'white'}`} size={25} className='metriq-navbar-social-icon' /> </Nav.Link>
+      <Nav.Link href='http://discord.unitary.fund' eventKey='9'> <FaDiscord color={`${'white'}`} size={25} className='metriq-navbar-social-icon' /> </Nav.Link>
+      <Nav.Link href='https://github.com/unitaryfund/metriq-app' eventKey='10'> <FaGithub color={`${'white'}`} size={25} className='metriq-navbar-social-icon' /> </Nav.Link>
     <MainSearchBar />
     <NavDropdown title='Account' active='true' className='metriq-navbar-text' alignRight>
-      <NavDropdown.Item as={Link} to='/Profile' eventKey='15'><p className='font-weight-bold'>Settings</p></NavDropdown.Item>
-      <NavDropdown.Item as={Link} to='/AddSubmission' eventKey='16'><p className='font-weight-bold'>Add Submission</p></NavDropdown.Item>
-      <NavDropdown.Item as={Link} to='/MySubmissions' eventKey='17'><p className='font-weight-bold'>My Submissions</p></NavDropdown.Item>
-      <NavDropdown.Item as={Link} to='/Token' eventKey='18'><p className='font-weight-bold'>API Token</p></NavDropdown.Item>
-      <NavDropdown.Item as={Link} to='/Password' eventKey='19'><p className='font-weight-bold'>Change password</p></NavDropdown.Item>
+      <NavDropdown.Item as={Link} to='/Profile' eventKey='13'><p className='font-weight-bold'>Settings</p></NavDropdown.Item>
+      <NavDropdown.Item as={Link} to='/AddSubmission' eventKey='14'><p className='font-weight-bold'>Add Submission</p></NavDropdown.Item>
+      <NavDropdown.Item as={Link} to='/MySubmissions' eventKey='15'><p className='font-weight-bold'>My Submissions</p></NavDropdown.Item>
+      <NavDropdown.Item as={Link} to='/Token' eventKey='16'><p className='font-weight-bold'>API Token</p></NavDropdown.Item>
+      <NavDropdown.Item as={Link} to='/Password' eventKey='17'><p className='font-weight-bold'>Change password</p></NavDropdown.Item>
       <NavDropdown.Divider />
-      <NavDropdown.Item onClick={handleOnClick} eventKey='20'><p className='font-weight-bold'>Logout</p></NavDropdown.Item>
+      <NavDropdown.Item onClick={handleOnClick} eventKey='18'><p className='font-weight-bold'>Logout</p></NavDropdown.Item>
     </NavDropdown>
-    <Nav.Link as={Link} to='/AddSubmission' eventKey='14'><Button variant='primary' className='metriq-navbar-button submit-button'>Submit</Button></Nav.Link>
+    <Nav.Link as={Link} to='/AddSubmission' eventKey='19'><Button variant='primary' className='metriq-navbar-button submit-button'>Submit</Button></Nav.Link>
   </Nav>
 
 export default AuthNavRight
